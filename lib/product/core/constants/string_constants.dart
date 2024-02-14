e3adece31bbe879fff4e9cc1b69351e2b2ee9e1c
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-14 23:41:37 +0300

    Add StringConstants class with predefined string values

---
+++ lib/product/core/constants/string_constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+@immutable
+
+/// StringConstants
+final class StringConstants {
+  const StringConstants._();
+
+  /// Json path
+  static const String jsonPath = 'assets/destination_list.json';
+
+  /// Visited Cache Key
+  static const String visited = 'visited';
+
+  /// App Title
+  static const String appTitle = 'Shared Preferences Case';
+}
